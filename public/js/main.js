d244126c228ee30e2857ade706b1e1172dc7caa0
Kalu David <kaludavid411@gmail.com>
2025-01-03 15:21:29 +0100

    added a bunch of comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,37 @@
-const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const deleteBtn = document.querySelectorAll('.fa-trash') //Storing all elements with a class of .fa-trash in a variable
+const item = document.querySelectorAll('.item span') //storing all span tags within a parent element with the class of item to a variable
+const itemCompleted = document.querySelectorAll('.item span.completed') // Selecting all span tags with a class of completed which are within a parent element with the class of item and storing them in a variable
 
-Array.from(deleteBtn).forEach((element)=>{
-    element.addEventListener('click', deleteItem)
-})
+Array.from(deleteBtn).forEach((element)=>{ //Creating an array from our selection and starting a loop 
+    element.addEventListener('click', deleteItem) //Add an event listener to the current item that waits for a click and then calls a function called deleteItem 
+}) //Close the loop
 
-Array.from(item).forEach((element)=>{
-    element.addEventListener('click', markComplete)
-})
+Array.from(item).forEach((element)=>{ //Creating an array from our selection and starting a loop 
+    element.addEventListener('click', markComplete)//Add an event listener to the current item that waits for a click and then calls a function called markComplete 
+}) //Close the loop
 
-Array.from(itemCompleted).forEach((element)=>{
-    element.addEventListener('click', markUnComplete)
-})
+Array.from(itemCompleted).forEach((element)=>{ //Creating an array from our selection and starting a loop 
+    element.addEventListener('click', markUnComplete) //Add an event listener to only completes items
+}) // Close the loop
 
-async function deleteItem(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('deleteItem', {
-            method: 'delete',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-              'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function deleteItem(){ //declare an asynchronous function
+    const itemText = this.parentNode.childNodes[1].innerText //looks inside of the list item and grabs only the inner text within the list span
+    try{ //starting a try block
+        const response = await fetch('deleteItem', { // creating a response variable that waits on a fetch to get data from the result of the delete route
+            method: 'delete', //sets the crud method for the route
+            headers: {'Content-Type': 'application/json'}, //specifying the type of content expected which is json
+            body: JSON.stringify({ //declare the message content being passed and stringify that content
+              'itemFromJS': itemText //setting the content of the body to the inner text of the list item and naming it itemFromJs
+            }) //closing the body
+          }) //closing the object
+        const data = await response.json() //waiting on json from the response to be converted
+        console.log(data) //log the data to the console
+        location.reload() //reloads the page to update what is displayed
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ //if an error occurs pass the error into the catch block
+        console.log(err) //log the error to the console
+    } //close the catch block
+} //end the function
 
 async function markComplete(){
     const itemText = this.parentNode.childNodes[1].innerText
