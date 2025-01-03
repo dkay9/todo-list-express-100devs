963d285c0522fff660486042746f43cede079467
Kalu David <kaludavid411@gmail.com>
2025-01-03 15:23:25 +0100

    new comments

--- public/js/main.js
+++ public/js/main.js
@@ -25,7 +25,7 @@ async function deleteItem(){ //declare an asynchronous function
             }) //closing the body
           }) //closing the object
         const data = await response.json() //waiting on json from the response to be converted
-        console.log(data) //log the data to the console
+        console.log(data) //log the result to the console
         location.reload() //reloads the page to update what is displayed
 
     }catch(err){ //if an error occurs pass the error into the catch block
@@ -33,40 +33,40 @@ async function deleteItem(){ //declare an asynchronous function
     } //close the catch block
 } //end the function
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function markComplete(){ //declare an asynchronous function
+    const itemText = this.parentNode.childNodes[1].innerText //looks inside of the list item and grabs only the inner text within the list span
+    try{ // starting a try block
+        const response = await fetch('markComplete', { // creates a response variable that waits on a fetch to get data from the result of markComplete route
+            method: 'put', //setting the crud method to "update" for the route 
+            headers: {'Content-Type': 'application/json'}, //specifying the type of content expected, which is json
+            body: JSON.stringify({ //declare the message content being passed and stringify that content
+                'itemFromJS': itemText //setting the content of the body to the inner text of the list item and naming it itemFromJs
+            }) //closing the body
+        }) //closing the object
+        const data = await response.json() //waiting on json from the response to be converted
+        console.log(data) //log the result to the console
+        location.reload() //reloads the page to update what is displayed
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ //if an error occurs pass the error into the catch block
+        console.log(err) //log the error to the console
+    } //close the catch block
+} //end the function
 
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+async function markUnComplete(){ //declare an asynchronous function
+    const itemText = this.parentNode.childNodes[1].innerText //looks inside of the list item and grabs only the inner text within the list span
+    try{ // starting a try block
+        const response = await fetch('markUnComplete', { // creates a response variable that waits on a fetch to get data from the result of markUnComplete route
+            method: 'put', //setting the crud method to "update" for the route 
+            headers: {'Content-Type': 'application/json'},  //specifying the type of content expected, which is json
+            body: JSON.stringify({ //declare the message content being passed and stringify that content
+                'itemFromJS': itemText //setting the content of the body to the inner text of the list item and naming it itemFromJs
+            }) //closing the body
+        }) //closing the object
+        const data = await response.json() //waiting on json from the response to be converted
+        console.log(data) //log the result to the console
+        location.reload() //reloads the page to update what is displayed
 
-    }catch(err){
-        console.log(err)
-    }
-}
+    }catch(err){ //if an error occurs pass the error into the catch block
+        console.log(err) //log the error to the console
+    } //close the catch block
+} //end the function
